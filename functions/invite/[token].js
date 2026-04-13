c5509c7f13f36b578f1769c385282e6d5b103ed8
dbyler37 <dara.byler@gmail.com>
2026-04-13 19:24:08 -0400

    Update [token].js

--- functions/invite/[token].js
+++ functions/invite/[token].js
@@ -1,1 +1,6 @@
-
+export async function onRequest(context) {
+  const response = await context.env.ASSETS.fetch(
+    new Request('https://hodgepodge.social/invite/index.html')
+  );
+  return response;
+}
